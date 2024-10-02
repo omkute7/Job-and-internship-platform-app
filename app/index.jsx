db9fcb72f0af5e974917aa61fde988a4350c6f43
sk <sk@gmail.com>
2024-10-02 16:24:29 +0530

    we can now use tailwind css

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react';
+import { View, Text } from "react-native";
+import React from "react";
 
 const App = () => {
   return (
-    <View style = {{flex:1, justifyContent:'center', alignItems:'center'}}>
+    <View>
       <Text>App</Text>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
